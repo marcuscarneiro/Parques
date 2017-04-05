28c40737aaa6230dfb353be329f99e409aad073b
Marcus Carneiro <marcuscarneiro@outlook.com>
2017-04-05 13:05:20 -0300

    Get notes by id

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,15 @@
 module.exports = function(app, db) {
+	app.get('/notes/:id', (req, res) => {
+		const details = { '_id': };
+		db collection('notes').findOne(details, (err, item) => {
+			if(err){
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			}
+		});
+	});
+
 	app.post('/notes', (req, res) => {
 		const note = { text: req.body.body, title: req.body.title };
 		db.collection('notes').insert(note, (err, result) => {
