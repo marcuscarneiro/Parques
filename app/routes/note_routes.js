2ce8db4873564e298a5bcb318a2002fc59be818b
Marcus Carneiro <marcuscarneiro@outlook.com>
2017-04-20 12:00:54 -0300

    Get note

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,10 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
 	app.get('/notes/:id', (req, res) => {
-		const details = { '_id': };
-		db collection('notes').findOne(details, (err, item) => {
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id) };
+		db.collection('notes').findOne(details, (err, item) => {
 			if(err){
 				res.send({'error':'An error has occurred'});
 			} else {
